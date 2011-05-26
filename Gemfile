ac111650affa341fcda2237ea695327c732cafb8
Brice Stacey <bricestacey@gmail.com>
2011-05-26 16:14:28 -0400

    Initial commit.

---
+++ Gemfile
@@ -0,0 +1,23 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.7'
+
+gem 'devise'
+gem 'cancan'
+
+gem 'haml'
+gem 'kaminari'
+gem 'formtastic'
+
+group :development, :test do
+  gem 'rspec-rails'
+  gem 'sqlite3'
+end
+
+group :test do
+  gem 'database_cleaner'
+  gem 'cucumber-rails'
+  gem 'valid_attribute'
+  gem 'factory_girl_rails'
+  gem 'capybara'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+DeskStats::Application.config.secret_token = '39732cccc110c256f780ce210f7f45a728dd33ca083be3f59d54f961bf2cdefdbc31f54f4aa82017b2350baaf9f422a03bcc6b644fc0aa417a14e6c0c98237e7'
